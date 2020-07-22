da30045f8423e2bd968aa7b09267c7f364a826fd
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-07-22 17:10:49 -0300

    Modelando

---
+++ app/Http/Controllers/RecomendacaoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Farmaco;
+use http\Env\Response;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class RecomendacaoController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            Farmaco::create($request->all());
+            return response()->json(['data'=>'store success!!']);
+        }catch (QueryException $e){
+            return response()->json(['data'=>'store erro!!']);
+        }
+    }
+
+    public function update(Request $request, $rec)
+    {
+        try {
+            $rec = Farmaco::find($rec);
+            $rec->update($request->all());
+            return response()->json(['data'=>'update success!!']);
+        }catch (QueryException $e){
+            return response()->json(['data'=>'update erro!!']);
+        }
+    }
+
+    public function delete($rec)
+    {
+        try{
+
+        }
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,25 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
 $router->group(['prefix'=>'farmacos'], function () use($router){
     $router->get('/', 'FarmacoController@index');
     $router->get('/{farmaco}', 'FarmacoController@show');
+    $router->post('/', 'FarmacoController@store');
+    $router->put('/{farmaco}', 'FarmacoController@update');
+    $router->delete('/{farmaco}', 'FarmacoController@delete');
+    /* Retorna um Inner Join entre duas tabelas*/
+    $router->get('/{farmaco}/recomendacao', 'FarmacoController@farmacoRecomendacao');
+
+});
+
+$router->group(['prefix'=>'recomendacao'], function() use($router){
+    $router->post('/', 'RecomendacaoController@store');
+    $router->put('/{rec}', 'RecomendacaoController@update');
+    $router->delete('/{rec}', 'RecomendacaoController@delete');
+});
+
+$router->group(['prefix'=>'dose'], function () use($router){
+    $router->post('/', 'DoseController@store');
+    $router->put('/{dose}', 'DoseController@update');
+    $router->delete('/{dose}', 'DoseController@delete');
 });
