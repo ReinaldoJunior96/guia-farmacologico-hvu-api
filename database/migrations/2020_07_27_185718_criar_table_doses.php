d5825c2ca67ffb3e95b2a9fca4a7e77d7ca0add6
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-07-27 16:41:44 -0300

    Migrations

--- database/migrations/2020_07_27_185718_criar_table_doses.php
+++ database/migrations/2020_07_27_185718_criar_table_doses.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CriarTabelaTblRecomendacoes extends Migration
+class CriarTableDoses extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CriarTabelaTblRecomendacoes extends Migration
      */
     public function up()
     {
-        Schema::create('tbl_recomendacoes', function (Blueprint $table) {
+        Schema::create('tbl_doses', function (Blueprint $table) {
             $table->id();
-            $table->
             $table->string("canino");
             $table->string("felino");
             $table->string("equino");
@@ -32,6 +31,6 @@ class CriarTabelaTblRecomendacoes extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tbl_recomendacoes');
+        Schema::dropIfExists('tbl_doses');
     }
 }
